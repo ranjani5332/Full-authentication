15cf65d49f23f8a00472d979eadb2db9ae2d23d4
ranjani5332 <wwwranjani147@.com>
2026-02-02 22:18:59 +0530

    git

--- backend/.server.js
+++ backend/.server.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+console.log("DEBUG MONGO_URI =",process.env.MONGO_URI);
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
