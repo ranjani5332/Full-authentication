a3340054d6504df06ea215a41de42ff5b5d3d1f2
ranjani5332 <wwwranjani147@.com>
2026-02-03 15:03:34 +0530

    git

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -6,23 +7,22 @@ const cors = require("cors");
 const authRoutes = require("./routes/auth");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", authRoutes);
-
-console.log("MONGO URI FROM ENV =",process.env.MONGO_URI);
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("MongoDB connection failed:", err);
-    process.exit(1);
-  });
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.error("❌ MongoDB Error:", err));
+
+app.use("/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("LMS Auth API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
